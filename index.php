f3af7187e2a845054b508dfa513f1f6a1525272b
Labogreed <valgustovictor@gmail.com>
2024-11-29 16:05:03 -0300

    receita

---
+++ index.php
@@ -0,0 +1,32 @@
+<h2>Receita: 
+<?php
+echo "Mousse de maracuja";
+?>
+</h2>
+<table>
+    <thead>
+        <tr>
+            <th>Ingredientes</th>
+            <th>Quantidade</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Leite Condensado</td>
+            <td>1 Lata</td>
+        </tr>
+        <tr>
+            <td>Suco de Maracuja</td>
+            <td>1 Lata</td>
+        </tr>
+        <tr>
+            <td>Creme de leite sem soro</td>
+            <td>1 Lata</td>
+        </tr>
+    </tbody>
+</table>
+<h1>Nota: </h1>
+<script>
+    var h1 = document.querySelector("h1");
+    h1.innerHTML= "Nota:10";
+</script>
